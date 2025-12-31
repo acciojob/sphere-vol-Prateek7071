961860c1249c6c6d806bfaa6182a1aeb9ad399b8
Prateek <106758759+Prateek7071@users.noreply.github.com>
2025-12-31 21:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function volume_sphere() {
     //Write your code here
-  
+	const radius = document.querySelector('#radius')
+	const volume = document.querySelector('#volume')
+	const submit = document.querySelector('#submit')
+	let vol = 0
+	submit.addEventListener('click',(e)=>{
+		e.preventDefault()
+		let Radius = parseInt(radius.value)
+		if(Radius>=0){
+			vol = (4*Math.PI*Radius*Radius*Radius/3).toFixed(4)
+			volume.value = vol
+		}
+		else{
+			volume.textContent = `NaN`
+		}
+	})
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
